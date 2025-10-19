8ba279b7d479788e1715be623cf9ef2e28abaa46
Eliel Vipata <vkilembo@gmail.com>
2025-10-19 14:38:04 -0400

    script to install llvm dependencies

---
+++ llvm/install_deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## This script installs all llvm dependencies for development on a debain based distro. Used to install dependencies for the Jewel Programming language.
+
+sudo apt install llvm
+sudo apt install llvm-dev
+sudo apt install curl
+sudo apt install libcurl4-openssl-dev
+sudo apt install libzstd-dev
+sudo apt install zlib1g-dev
+sudo apt install libedit-dev
